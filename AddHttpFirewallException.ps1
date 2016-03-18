fe578ce650c7fcefda355f020b9cb3e5904c3a43
Buc Rogers <brogersyh@yahoo.com>
2016-03-17 20:02:32 -0500

    remove param

--- AddHttpFirewallException.ps1
+++ AddHttpFirewallException.ps1
@@ -3,10 +3,6 @@
 # Adds firewall exception for http
 
 #  Arguments : HostName, specifies the FQDN of machine or domain
-param
-(
-    [string] $HostName = $(throw "HostName is required.")
-)
 
 if (!(Get-NetFirewallRule | where {$_.Name -eq "Http"})) {
     New-NetFirewallRule -Name "Http" -DisplayName "Http" -Protocol tcp -LocalPort 80 -Action Allow -Enabled True
